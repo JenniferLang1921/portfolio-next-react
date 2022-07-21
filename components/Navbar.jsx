8e312ee892f2c72350fd3fed4de3066d8903b234
Jennifer Lang <jenniferlang1921@gmail.com>
2022-07-21 13:27:39 -0500

    navborders, favicon

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -67,27 +67,27 @@ const Navbar = () => {
         <div>
           <ul style={{ color: `${linkColor}` }} className="hidden md:flex">
             <Link href="/">
-              <li className="ml-10 text-sm uppercase font-semibold hover:border-b">
+              <li className="ml-10 text-sm border-[#5651e5] uppercase font-semibold hover:border-b">
                 Home
               </li>
             </Link>
             <Link href="/#about">
-              <li className="ml-10 text-sm uppercase font-semibold hover:border-b">
+              <li className="ml-10 text-sm border-[#5651e5] uppercase font-semibold hover:border-b">
                 About
               </li>
             </Link>
             <Link href="/#skills">
-              <li className="ml-10 text-sm uppercase font-semibold hover:border-b">
+              <li className="ml-10 text-sm border-[#5651e5] uppercase font-semibold hover:border-b">
                 Skills
               </li>
             </Link>
             <Link href="/#projects">
-              <li className="ml-10 text-sm uppercase font-semibold hover:border-b">
+              <li className="ml-10 text-sm border-[#5651e5] uppercase font-semibold hover:border-b">
                 Projects
               </li>
             </Link>
             <Link href="/#contact">
-              <li className="ml-10 text-sm font-semibold uppercase hover:border-b">
+              <li className="ml-10 text-sm border-[#5651e5] font-semibold uppercase hover:border-b">
                 Contact
               </li>
             </Link>
