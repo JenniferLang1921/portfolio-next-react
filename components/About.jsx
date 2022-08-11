8b3bc8153b93f762a7adf4c597fcbac1d0e3996f
Jennifer Lang <jenniferlang1921@gmail.com>
2022-08-10 22:54:46 -0500

    font change social media icons

--- components/About.jsx
+++ components/About.jsx
@@ -17,19 +17,10 @@ const About = () => {
             I earned my certificate at Full-Stack Web Development Coding
             Bootcamp at the University of Kansas in 2018. Since then, I have
             been working as a front-end drupal and full-stack wordpress
-            developer for Agile Technology Solutions at the University of Kansas
-            and am currently working there. I work with javascript, html, css, and php.
+            developer for Agile Technology Solutions at the University of
+            Kansas.
           </p>
-          <p className="py-2 text-gray-600">
-            I would like to transition into a job that includes building apps
-            with React.js, Vue.js, and Next.js and work on a great team. I have
-            exceptional experience with accessibility, SEO, and product
-            marketing. I love creating and problem solving in programming. I want to help others create
-            websites they love, make them money, and find solutions for their
-            needs. </p>
-            <p>When I'm not coding, you'll find me painting, watching
-            youtube, reading a book, or exploring lakes and cities.
-          </p>
+
           <Link href="/#projects">
             <p className="py-2 text-gray-600 underline cursor-pointer">
               Check out some of my latest projects.
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { AiOutlineMail } from "react-icons/ai";
-import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { FaLinkedinIn, FaGithub, FaTwitter } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-
+import { AiOutlineBehance } from "react-icons/ai";
 const Main = () => {
   return (
     <div id="home" className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <p className="uppercase text-sm tracking-widest text-gray-600">
+          <p className="uppercase text-sm tracking-widest text-gray-900">
             Let's build something together
           </p>
           <h1 className="py-4 text-gray-700">
@@ -22,7 +22,7 @@ const Main = () => {
             learning MySQL and node.js back-end technologies.
           </p>
 
-          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+          <div className="flex items-center justify-between max-w-[500px] m-auto py-4">
             <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-75">
               <a href="https://www.linkedin.com/in/jenniferlang1921/">
                 <FaLinkedinIn />
@@ -34,10 +34,21 @@ const Main = () => {
               </a>
             </div>
             <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-75">
+              <a href="https://twitter.com/JennyLang01">
+                <FaTwitter />
+              </a>
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-75">
+              <a href="https://www.behance.net/jenniferlang1921">
+                <AiOutlineBehance />
+              </a>
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-75">
               <a href="mailto:jenniferlang1921@gmail.com">
                 <AiOutlineMail />
               </a>
             </div>
+
             <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-75">
               <a href="https://drive.google.com/file/d/1STLk7Q5M935ZVccIr9x5dUeicDkI9T0M/view?usp=sharing">
                 <BsFillPersonLinesFill />
